1b30fd488a089943b7381d4c26258f48167e876d
yg0103 <yg0103@woozoom.net>
2019-09-03 18:54:05 +0800

    增加malloc

--- app/platform/linux/main.c
+++ app/platform/linux/main.c
@@ -5,7 +5,7 @@
 #include "rtos.h"
 #include "cmd.h"
 #include "fatfs.h"
-
+#include "queue.h"
 
 void inode_handler(inode_t* node, struct inode_path_s *info)
 {
@@ -40,17 +40,64 @@ void proc_test()
     cat("/proc/uptime");
 }
 
+
+
+// void sq_add_first(yg_sq_queue_t*queue, yg_sq_entry_t *node)
+// {   
+//     node->next = queue->head;
+//     if (!queue->head) {
+//         queue->tail = node;
+//         queue->head = node;
+//     } else {
+//         queue->head = node;
+//     }
+// }
+
+
+
+struct name{
+    struct name *next; 
+    int number;
+};
+yg_sq_queue_t name_queue;
+
+struct name table[5] = {
+    {NULL, 1},
+    {NULL, 2},
+    {NULL, 3},
+    {NULL, 4},
+    {NULL, 5}
+};
+
+void test(void)
+{  
+    sq_init(&name_queue);
+
+    for (int i = 0; i < 5; i++) {
+        yg_sq_add_tail(&name_queue, (yg_sq_entry_t *)&table[i]);
+    }
+
+    for (int i = 0; i < 5; i++) {
+        yg_sq_remove_first(&name_queue);
+    }
+}
+
+
 int main(int argc, char **argv)
 {   
-    ygos_init();
-    ygos_inode_list_init();
-    ramlog_register();
+    mem_init();
+
+    // ygos_init();
+    // ygos_inode_list_init();
+    // ramlog_register();
     
-    if (!mount(NULL, "/proc", "procfs", 0, NULL)) {
-        printf("mount ok!\n");
-    }
+    // if (!mount(NULL, "/proc", "procfs", 0, NULL)) {
+    //     printf("mount ok!\n");
+    // }
+
     
-    fat32_test();
+    
+    // fat32_test();
 
 	return 0;
 }
---
+++ src/queue.h
@@ -0,0 +1,60 @@
+#ifndef _QUEUE_H
+#define _QUEUE_H
+
+#define NULL ((void*)0)
+
+struct yg_sq_entry_s
+{
+   struct yg_sq_entry_s *next;
+};
+typedef struct yg_sq_entry_s yg_sq_entry_t;
+
+struct yg_sq_queue_s
+{
+   yg_sq_entry_t *head;
+   yg_sq_entry_t *tail;
+};
+typedef struct yg_sq_queue_s  yg_sq_queue_t;
+
+static inline void sq_init(yg_sq_queue_t*queue)
+{   
+    queue->head = NULL;
+    queue->tail = NULL;
+}
+
+static inline yg_sq_entry_t *yg_sq_remove_first(yg_sq_queue_t*queue)
+{   
+    yg_sq_entry_t *node;
+    
+    //取出头节点
+    node = queue->head;
+    if (!queue->head) {
+       return NULL;
+    }
+    
+    //将头指针的下一个节点，作为头节点
+    queue->head = queue->head->next;
+    if( !queue->head ) {
+        queue->tail = NULL;
+    }
+
+    node->next = NULL;
+    return node;
+}
+
+static inline yg_sq_add_tail(yg_sq_queue_t*queue, yg_sq_entry_t *node)
+{   
+    node->next = NULL;
+    if (!queue->head) {
+        //第一次
+        queue->head = node;
+        queue->tail = node;
+    } else {
+        
+        //当前尾指针的next指向下一个节点，移动尾指针到下一个节点
+        queue->tail->next = node;
+        queue->tail = node;  
+    }
+}
+
+#endif
